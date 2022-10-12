49358180a6be2b516d36347b2ce66554592d25da
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-10-12 14:56:46 -0300

    add tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,38 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+
+    colors: {
+      white: '#fff',
+      black: '#000',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      cyan: {
+        500: '#81d8f7',
+        300: '#9b31fb',
+      }
+    },
+
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      }
+    },
   },
   plugins: [],
 }
