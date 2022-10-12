66c06922449cce831d8669228af9e14be114b664
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-10-12 17:47:10 -0300

    add text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
+
+export interface TextProps {
+	children?: ReactNode;
+	size?: 'sm' | 'md' | 'lg';
+	asChild?: boolean;
+}
+
+export function Text({ children, asChild, size = 'md' }: TextProps) {
+	const Comp = asChild ? Slot : 'span';
+
+	return (
+		<Comp
+			className={clsx('text-gray-100 font-sans', {
+				'text-xs': size === 'sm',
+				'text-sm': size === 'md',
+				'text-md': size === 'lg',
+			})}
+		>
+			{ children }
+		</Comp>
+	)
+}
