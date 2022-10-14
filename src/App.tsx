a5b657c2e2fae692605ecb94c1b697898b63a53a
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-10-13 22:30:55 -0300

    change sample example

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,55 @@
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
 import { Heading } from './components/Heading';
 import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
 import './styles/global.css';
 
 export function App() {
   return (
-    <div className='w-screen h-screen p-12 bg-gray-900'>
-      <section>
-        <Heading size='lg'>
-          <h1>Lab Design System</h1>
-        </Heading>
+    <div className='flex flex-col items-center justify-center w-screen h-screen p-12 bg-gray-900'>
+      <Heading size='lg'>
+        <h1>Login</h1>
+      </Heading>
 
-        <Text>
-          <p>Criando um Design System a partir do Figma utilizando o Storybook.</p>
-        </Text>
-      </section>
+      <form className='flex flex-col gap-3 w-full max-w-sm'>
+        <div className='flex flex-col gap-1'>
+          <Text asChild size='sm'>
+            <label htmlFor="mail">Email</label>
+          </Text>
+          <TextInput.Root className='w-full'>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input id='mail' type='email' placeholder='Digite seu e-mail' />
+          </TextInput.Root>
+        </div>
 
-      <section className='mt-6'>
-        <Heading>Acessando o Storybook</Heading>
-        <Text>
-          <p>
-            Para acessar o Storybook, execute 
-            <code className='ml-1 px-[6px] py-[1px] rounded italic bg-gray-800'>
-              yarn storybook
-            </code>.
-          </p>
+				<div className='flex flex-col gap-1'>
+          <Text asChild size='sm'>
+            <label htmlFor="pass">Senha</label>
+          </Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input id='pass' type='password' placeholder='*********' />
+          </TextInput.Root>
+        </div>
+
+        <Text asChild>
+          <label htmlFor='confirm' className='flex items-center gap-2 my-3'>
+            <Checkbox id='confirm' />
+
+            Confirmo com os termos de uso
+          </label>
         </Text>
-      </section>
+
+        <Button>
+          Criar minha conta
+        </Button>
+      </form>
     </div>
   )
 }
